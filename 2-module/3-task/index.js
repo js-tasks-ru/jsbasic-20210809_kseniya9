5e6515c9868dcb600c248c2b5e261052fde24fd4
Kseniya Fedoseeva <fedoseeva.datsenko@gmail.com>
2021-08-18 19:24:07 +0300

    калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,20 @@
 let calculator = {
-  // ваш код
+  num1: 0,
+  num2: 0,
+  read(a, b) {
+    this.num1 = a;
+    this.num2 = b;
+  },
+  sum() {
+    return this.num1 + this.num2;
+  },
+  mul() {
+    return this.num1 * this.num2;
+  },
 };
 
+calculator.read();
+calculator.sum();
+calculator.mul();
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
