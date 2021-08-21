ab869adc71f675c5e56b28cd20e3b9f32aa61bb9
Kseniya Fedoseeva <fedoseeva.datsenko@gmail.com>
2021-08-21 23:44:26 +0300

    задача 3-3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
-  // ваш код...
+  return str
+    .split("-")
+    .map((item, index) => {
+      if (index > 0) {
+        return item.charAt(0).toUpperCase() + item.slice(1);
+      } else {
+        return item;
+      }
+    })
+    .join("");
 }
