5898f3c074da7094aa93fb2488f73b9f01dc5c68
Kseniya Fedoseeva <fedoseeva.datsenko@gmail.com>
2021-08-25 13:45:03 +0300

    задача 4-2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,6 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let rows = Array.from(table.rows);
+  for (let i = 0; i < rows.length; i++) {
+    rows[i].cells[i].style.backgroundColor = "red";
+  }
 }
