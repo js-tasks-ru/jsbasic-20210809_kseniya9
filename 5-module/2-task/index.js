dacf5a2373852f3d6f7b9fefb8b26a7b03d44989
Kseniya Fedoseeva <fedoseeva.datsenko@gmail.com>
2021-09-01 15:27:23 +0300

    задача 5-2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  const btnTextHide = document.querySelector(".toggle-text-button");
+  const text = document.querySelector("#text");
+  btnTextHide.addEventListener("click", function () {
+    if (!text.hidden) {
+      text.hidden = true;
+    } else {
+      text.hidden = false;
+    }
+  });
 }
