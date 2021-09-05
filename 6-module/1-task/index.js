8a58e8e10bc897483adb86017bf082493d7129f6
Kseniya Fedoseeva <fedoseeva.datsenko@gmail.com>
2021-09-06 00:31:07 +0300

    задача 6-1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,26 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.render();
+  }
+  render() {
+    const table = document.createElement("table");
+    for (let row of this.rows) {
+      const tableRow = `<tr>
+      <td>${row.name}</td>
+      <td>${row.age}</td>
+      <td>${row.salary}</td>
+      <td>${row.city}</td>
+      <td><button>X</button></td>
+      </tr>`;
+      table.insertAdjacentHTML("beforeend", tableRow);
+    }
+    table.addEventListener("click", function (event) {
+      if (event.target.tagName === "BUTTON") {
+        event.target.closest("tr").remove();
+      }
+    });
+    return table;
   }
 }
